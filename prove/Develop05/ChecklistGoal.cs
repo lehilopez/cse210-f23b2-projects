0131976edcdd91f4e457cd084a6bb5b8e5feb11c
LLS <lehilopez@yahoo.com>
2023-12-14 07:19:55 -0600

    Develop05

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -3,6 +3,12 @@ public class ChecklistGoal : Goal
     private int _amountCompleted;
     private int _target;
     private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+    }
     
     public override void RecordEvent()
     {
--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,6 +1,9 @@
 public class EternalGoal : Goal
 {
-    
+    public EternalGoal(string name, string description, int points) : base(name, description, points)
+    {
+        
+    }
     
     public override void RecordEvent()
     {
